5b2a3391b1553550cc657b5b47b316d5aae57163
jajecnikmarci <jajecnikmarcell2004@gmail.com>
2023-05-27 11:57:28 +0200

    Done

--- NHF/skeleton/skeletondoxy/html/search/all_4.js
+++ NHF/skeleton/skeletondoxy/html/search/all_4.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['date_0',['Date',['../class_date.html',1,'Date'],['../class_date.html#a5874432b5f7339205866055201e885d7',1,'Date::Date()']]],
-  ['deleteclient_1',['deleteClient',['../class_m_v_m.html#a85592d6dbe4adadb6fe4cbfb1b4861f2',1,'MVM']]]
+  ['deleteclient_1',['deleteClient',['../class_m_v_m.html#aa1e7db7cfe6de5a5c2bca90a1b9cce84',1,'MVM']]]
 ];
--- NHF/skeleton/skeletondoxy/html/search/functions_b.js
+++ NHF/skeleton/skeletondoxy/html/search/functions_b.js
@@ -6,7 +6,7 @@ var searchData=
   ['setctime_3',['setCtime',['../class_contract.html#aaf9ae551436a51ac41d2f08149366c79',1,'Contract']]],
   ['setctype_4',['setCtype',['../class_contract.html#a96cec0aff3df76236cc75e3bf63ef5ff',1,'Contract']]],
   ['setend_5',['setEnd',['../class_contract__date.html#a2a5399e4a07a2f59ca4e67d88f328242',1,'Contract_date']]],
-  ['setid_6',['setID',['../class_person.html#a87486d9388f3cc56003e3a915d58fe72',1,'Person::setID()'],['../class_company.html#a81fa31e65ff4161fc6e782f2067975c4',1,'Company::setID()']]],
+  ['setid_6',['setID',['../class_person.html#a48bd1dda5186f86d5ea09ba0fb47b87a',1,'Person::setID()'],['../class_company.html#a6e08c872488ba07ff4ede28930116c8c',1,'Company::setID()']]],
   ['setlast_5finvoicing_7',['setLast_invoicing',['../class_contract.html#a986868745160d0e296d894ee2bd88178',1,'Contract']]],
   ['settariff_8',['setTariff',['../class_contract.html#ab5517c38f4f1d68e597071ae0a9e8b66',1,'Contract']]],
   ['size_9',['size',['../class_my_vector.html#ac167e7f4cf1f1f6a01053db2e5efd3a2',1,'MyVector']]]
